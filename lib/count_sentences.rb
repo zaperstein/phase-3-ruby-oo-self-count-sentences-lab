6d3dcca7ce1bcea2098a12f6a83cf24ca998edfa
Maggie Saperstein <maggie.saperstein@gmail.com>
2022-07-03 06:43:00 -0500

    commit tree 855dbafe440ca7a49f598e82481d7eca7fbdf373

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    #return the number of methods that monkey-patched methods that return true
+    split_sentences = self.split(/[.?!]/)
+    condensed_split_sentences = split_sentences.reject{|element| element.empty?}
+    condensed_split_sentences.count
   end
 end
